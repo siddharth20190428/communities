ff30fbe7035234eb0f5e92982c09cf477386352b
Siddharth <siddharth201820@gmail.com>
2023-12-27 17:25:33 +0530

    added models/user

---
+++ src/models/user.js
@@ -0,0 +1,34 @@
+const { Snowflake } = require("@theinternetfolks/snowflake");
+const mongoose = require("mongoose");
+
+const UserSchema = mongoose.Schema(
+    {
+        id: {
+            type: String,
+            primaryKey: true,
+            default: () => Snowflake.generate(),
+        },
+        name: {
+            type: String,
+            maxlength: 64,
+            default: null,
+            required: true,
+        },
+        email: {
+            type: String,
+            unique: true,
+            maxlength: 128,
+            required: true,
+        },
+        password: {
+            type: String,
+            maxlength: 64,
+            required: true,
+        },
+    },
+    { timestamps: true }
+);
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
