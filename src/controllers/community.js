6891aa929a4a73ea4c07c8e0a6c907b4e4340bdb
Siddharth <siddharth201820@gmail.com>
2023-12-29 23:58:21 +0530

    added /v1/member/me/member

--- src/controllers/community.js
+++ src/controllers/community.js
@@ -74,9 +74,10 @@ const getAllCommunityMembers = async (req, res) => {
     const PAGE_SIZE = 10;
     const page = 1;
     const skip = (page - 1) * PAGE_SIZE;
+    const filters = { community: communityId };
 
     // fetching all the members of the community and populating them with their name and role
-    const results = await Member.find({ community: communityId })
+    const results = await Member.find()
       .populate("user", "name")
       .populate("role", "name")
       .select("-updatedAt")
@@ -85,9 +86,7 @@ const getAllCommunityMembers = async (req, res) => {
       .exec();
 
     // totaldocuments and pages
-    const totalDocs = await Community.countDocuments({
-      community: communityId,
-    });
+    const totalDocs = await Community.countDocuments(filters);
     const totalPages = Math.ceil(totalDocs / PAGE_SIZE);
 
     res.apiSuccess(results, { total: totalDocs, pages: totalPages, page });
@@ -112,18 +111,62 @@ const getAllOwnedCommunities = async (req, res) => {
     const PAGE_SIZE = 10;
     const page = 1;
     const skip = (page - 1) * PAGE_SIZE;
+    const filters = { owner: currentUser.id };
 
     // fetching all communities which are owned by current user and populating them with their owners
-    const results = await Community.find({ owner: currentUser.id })
+    const results = await Community.find(filters)
       .skip(skip)
       .limit(PAGE_SIZE)
       .exec();
 
     // totaldocuments and pages
-    const totalDocs = await Community.countDocuments({ owner: currentUser.id });
+    const totalDocs = await Community.countDocuments(filters);
     const totalPages = Math.ceil(totalDocs / PAGE_SIZE);
 
     res.apiSuccess(results, { total: totalDocs, pages: totalPages, page });
+  } catch (error) {
+    res.apiError(error);
+    console.log(error);
+  }
+};
+
+const getAllJoinedCommunities = async (req, res) => {
+  try {
+    const { authorization } = req.headers;
+    // accessing current user
+    const currentUser = jwt.verify(authorization, process.env.JWT_SECRET);
+    if (!currentUser)
+      return res.apiError({
+        message: "You need to sign in to proceed.",
+        code: "NOT_SIGNEDIN",
+      });
+
+    // pagination variables
+    const PAGE_SIZE = 10;
+    const page = 1;
+    const skip = (page - 1) * PAGE_SIZE;
+    const filters = { user: currentUser.id };
+
+    // fetching all communityIds joined by the current user
+    const communityIds = await Member.find(filters, "community")
+      .skip(skip)
+      .limit(PAGE_SIZE)
+      .exec();
+    // creating an array of communityids
+    const communityIdsArray = communityIds.map((entry) => entry.community);
+
+    // fetching all communities data with communityIds and populating them with their owners
+    const communities = await Community.find({
+      _id: { $in: communityIdsArray },
+    }).populate("owner", "name");
+    console.log(communityIds);
+    console.log(communities);
+
+    // totaldocuments and pages
+    const totalDocs = await Member.countDocuments(filters);
+    const totalPages = Math.ceil(totalDocs / PAGE_SIZE);
+
+    res.apiSuccess(communities, { total: totalDocs, pages: totalPages, page });
   } catch (error) {
     res.apiError(error);
     console.log(error);
@@ -135,4 +178,5 @@ module.exports = {
   getAllCommunities,
   getAllCommunityMembers,
   getAllOwnedCommunities,
+  getAllJoinedCommunities,
 };
--- src/routes/community.js
+++ src/routes/community.js
@@ -4,6 +4,7 @@ const {
   getAllCommunities,
   getAllCommunityMembers,
   getAllOwnedCommunities,
+  getAllJoinedCommunities,
 } = require("../controllers/community");
 
 const router = express.Router();
@@ -12,5 +13,6 @@ router.post("/", createCommunity);
 router.get("/", getAllCommunities);
 router.get("/:id/members", getAllCommunityMembers);
 router.get("/me/owner", getAllOwnedCommunities);
+router.get("/me/member", getAllJoinedCommunities);
 
 module.exports = router;
